1cdddda8fc4bffcbee088c8a158a1b33ad3547c9
Eric Liang <etlgfx@gmail.com>
2013-12-08 01:22:01 -0500

    ton of fixes
    * style is implemented
    * floating clouds
    * corrected a lot of cat data
    * corrected more event detection fails

--- public/js/catmouse.js
+++ public/js/catmouse.js
@@ -4,40 +4,50 @@ var STATE_MOVING = 1,
 
 var cat = null,
 	coords = [],
-	lastCoords = [],
 	state = STATE_MOVING,
 	timeout = null,
 	multiplier = 1,
 	size = 1,
-	correction = 50;
+	correction = 50,
+    waitTimeout = null;
 
 function move(e) {
-	coords[0] = e.offsetX;
-	coords[1] = e.offsetY;
-	changeState(STATE_MOVING);
+	if (Math.abs(e.layerX - coords[0]) < 5 && Math.abs(e.layerY - coords[1]) < 5) {
+		return;
+	}
+
+	coords[0] = e.layerX;
+	coords[1] = e.layerY;
+
+    if (state != STATE_WAITING) {
+        changeState(STATE_MOVING);
+    }
 
 	clearTimeout(timeout);
-	timeout = setTimeout(catchit, 1000);
+	clearTimeout(waitTimeout);
+
+    waitTimeout = setTimeout(wait, 300);
+	timeout = setTimeout(catchit, 700);
+}
+
+function wait() {
+    changeState(STATE_WAITING);
+
+    document.querySelector('.progress .bar').style.width = '30%';
 }
 
 function catchit() {
-	/*-----------
-	 *   100
-	 *
-	 *     ---------------
-	 * 100 | 100
-	 *     |
-	 * |----
-	 */
 	if (coords.length == 2 && coords[0] && coords[1]) {
 		changeState(STATE_WAITING);
+
+        document.querySelector('.progress .bar').style.width = '100%';
 
 		loadCat(coords, function (closest) {
 			var catCoords = [size * closest.coords[0], size * closest.coords[1]];
 			var imgCoords = [catCoords[0] - correction, catCoords[1] - correction];
 			var offset = [(imgCoords[0] - catCoords[0]) + Math.max(-correction, Math.min(correction, coords[0] - imgCoords[0])), (imgCoords[1] - catCoords[1]) + Math.max(-correction, Math.min(correction, coords[1] - imgCoords[1]))];
 
-			console.log(catCoords, imgCoords, offset);
+			console.log(catCoords, imgCoords, offset, closest.filename);
 
 			changeState(STATE_DISPLAY);
 			cat.style.background = 'url(images/'+ closest.filename +')';
@@ -66,21 +76,53 @@ function loadCat(coords, callback) {
 function changeState(state) {
 	switch (state) {
 		case STATE_MOVING:
-			document.querySelector('#cat p.hint').style.display = 'none';
+			document.querySelector('#cat .hint').style.display = 'block';
 			cat.style.background = 'black';
 			break;
 
 		case STATE_WAITING:
-			var wait = document.querySelector('#cat p.wait');
+			var wait = document.querySelector('#cat .wait');
+			document.querySelector('#cat .hint').style.display = 'none';
 			wait.style.display = 'block';
 			wait.style.left = coords[0];
 			wait.style.top = coords[1];
 			break;
 
 		case STATE_DISPLAY:
-			document.querySelector('#cat p.wait').style.display = 'none';
+			document.querySelector('#cat .wait').style.display = 'none';
+            document.querySelector('.progress .bar').style.width = '0';
 			break;
 	}
+}
+
+var cloudClasses = ['cloud1', 'cloud2', 'cloud3', 'cloud4', 'cloud1cat', 'cloud2cat'];
+
+function clouds() {
+    var clds = document.querySelectorAll('.cloud');
+
+    for (var i = 0; i < clds.length; i++) {
+        if (clds[i].offsetLeft > window.innerWidth + clds[i].clientWidth) {
+            document.body.removeChild(clds[i]);
+            continue;
+        }
+
+        clds[i].style.left = (parseFloat(clds[i].style.left) + 2 - parseFloat(clds[i].style.top) / window.innerHeight) + "px";
+    }
+
+    if (clds.length < 8) {
+        var cld = document.body.appendChild(document.createElement('div'));
+        cld.classList.add('cloud');
+        cld.classList.add(cloudClasses[Math.floor(Math.random() * cloudClasses.length)]);
+
+        var y = (Math.random() * window.innerHeight);
+        var s = (Math.random() * 0.5 + 1 - y / window.innerHeight) * 0.6667;
+        cld.style.top = y + "px";
+        cld.style.left = -(Math.random() * window.innerWidth + 300) + "px";
+        cld.style.webkitTransform = "scale("+ s +")";
+        cld.style.mozTransform = "scale("+ s +")";
+    }
+
+    window.requestAnimationFrame(clouds);
 }
 
 function init() {
@@ -88,6 +130,7 @@ function init() {
 	multiplier = Number(cat.getAttribute('data-size')) / cat.clientWidth;
 	size = Number(cat.getAttribute('data-size'));
 
+    window.requestAnimationFrame(clouds);
 	cat.addEventListener('mousemove', move, true);
 	catchit();
 }
